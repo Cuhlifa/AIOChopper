c6da7791c4d204285daaf7ad120490184ab49f62
Connor <123@123.com>
2015-06-09 08:18:02 -0400

    first commit

--- src/scripts/AIOChopper.java
+++ src/scripts/AIOChopper.java
@@ -1,26 +1,41 @@
 package scripts;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
 import org.tribot.script.interfaces.Painting;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by fsfdsdf on 6/8/2015.
  */
 @ScriptManifest(authors = { "Peticca10" }, name = "AIOChopper++", category = "WoodCutting")
 public class AIOChopper extends Script implements Painting {
+
     @Override
     public void onPaint(Graphics graphics) {
 
     }
 
+    public static void main(String[] args) {
+        FXGUI.main(null);
+    }
+
     @Override
     public void run() {
-        try{
-            FXGUI.main(null);
-        }catch (Exception e){}
+        FXGUI.main(null);
     }
 
 }
--- src/scripts/FXGUI.java
+++ src/scripts/FXGUI.java
@@ -1,10 +1,16 @@
 package scripts;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import sun.nio.ch.ThreadPool;
 
 import java.net.URL;
 
@@ -20,11 +26,11 @@ public class FXGUI extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(new URL("https://raw.githubusercontent.com/Cuhlifa/AIOChopper/master/src/scripts/FXGUI.java"));
+        Parent root = FXMLLoader.load(new URL("https://raw.githubusercontent.com/Cuhlifa/AIOChopper/master/src/scripts/GUI.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("AIOChopper");
+;       primaryStage.setTitle("AIOChopper");
         primaryStage.show();
 
     }
